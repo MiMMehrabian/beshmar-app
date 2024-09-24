7f4064ddd96a2e6d544a5e3995b5665b96ccb91b
MiMMehrabian <86444040+MiMMehrabian@users.noreply.github.com>
2024-09-24 11:02:23 +0330

    fix : ui bugs

--- src/components/Layouts/DefaultLayout.tsx
+++ src/components/Layouts/DefaultLayout.tsx
@@ -18,7 +18,7 @@ export default function DefaultLayout({
         {/* <!-- ===== Sidebar End ===== --> */}
 
         {/* <!-- ===== Content Area Start ===== --> */}
-        <div className="relative flex flex-1 flex-col lg:mr-60">
+        <div className="relative flex flex-1 flex-col lg:mr-60 w-full">
           {/* <!-- ===== Header Start ===== --> */}
           <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           {/* <!-- ===== Header End ===== --> */}
